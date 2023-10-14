f56dd748078a91f4be4bd108e09357e9957de0b3
MJanowskiDev <mat.krz.janowski@gmail.com>
2023-10-14 10:17:00 +0200

    feat(frontend) - first use of commun-ui Input component

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,8 +1,16 @@
 import NxWelcome from './nx-welcome';
 
+import { Input } from '@ems/common-ui';
+
 export function App() {
   return (
     <div>
+      <Input
+        type="text"
+        id="default-input"
+        label="Text Input"
+        placeholder="Placeholder"
+      />
       <NxWelcome title="greetings" />
     </div>
   );
